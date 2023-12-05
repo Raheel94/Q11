c84f0538bc56881f8351462dd422f292bf121240
Raheel94 <muha.raheel94@gmail.com>
2023-12-05 13:24:26 +0500

    go

---
+++ index.ts
@@ -0,0 +1,10 @@
+//Names: Store the names of a few of your friends in a array called names.
+//Print each person’s name by accessing each element in the list, one at a time.
+
+// Declaring an array variable
+let names: string[] = ["Raheel","Amman","Zainub"]
+
+//To Print each element in the list using index
+console.log(names[0])
+console.log(names[1])
+console.log(names[2])
